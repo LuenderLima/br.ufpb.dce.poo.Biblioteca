45b1a9175ef3c0a0cc064322b401b1cad123ab27
José Luender de Lima Santos <jose.luender@dce.ufpb.br>
2014-04-22 17:14:03 -0300

    Update UsuarioComposto.java

--- br.ufpb.dce.poo-aps.Biblioteca/src/br/ufpb/dce/poo/projetopack/UsuarioComposto.java
+++ br.ufpb.dce.poo-aps.Biblioteca/src/br/ufpb/dce/poo/projetopack/UsuarioComposto.java
@@ -2,24 +2,24 @@ package br.ufpb.dce.poo.projetopack;
 
 import java.util.List;
 
-public abstract class UsuarioComposto implements Usuario {
+public abstract class Usuario {
+	
 	private List<Emprestimo> emprestimos;
 	private String nome;
 	private String matricula;
 	private String cpf;
-	private String periodoIngresso;
-	private String departamento;
-	private String curso;
 	
-	public UsuarioComposto(String nome, String matricula, String cpf){
+	public Usuario(String nome, String matricula, String cpf){
 		this.nome = nome;
 		this.matricula = matricula;
 		this.cpf = cpf;
+		this.emprestimos = new LinkedList<Emprestimo>();
 	}
 	
 	public void adicionarEmprestimo(Emprestimo e){
 		this.emprestimos.add(e);
 	}
+	
 	public void removerEmprestimo(Emprestimo emprestimo){
 		for(Emprestimo e: this.emprestimos){
 			if(e.equals(emprestimo)){
@@ -29,17 +29,6 @@ public abstract class UsuarioComposto implements Usuario {
 		}
 	}
 	
-	public void setPeriodoIngresso(String periodo){
-		this.periodoIngresso = periodo;
-	}
-	
-	public void setCurso(String curso){
-		this.curso = curso;
-	}
-	
-	public String getCurso(){
-		return this.curso;
-	}
 	public String getNome(){
 		return this.nome;
 	}
@@ -51,19 +40,6 @@ public abstract class UsuarioComposto implements Usuario {
 	}
 	public List<Emprestimo> getEmprestimos(){
 		return this.emprestimos;
-	}
-	
-	public String getPeriodoIngresso(){
-		return this.periodoIngresso;
-		
-	}
-	
-	public String getDepartamento(){
-		return this.departamento;
-	}
-	
-	public void setDepartamento(String departamento){
-		this.departamento = departamento;
 	}
 	
 	public abstract int getQuantDiasEmprestimo();
