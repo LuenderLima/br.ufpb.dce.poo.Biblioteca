2189f5a64f8a75b27fdbe7b63e056b663da13fa9
José Luender de Lima Santos <jose.luender@dce.ufpb.br>
2014-04-22 17:08:08 -0300

    Update Aluno.java

--- br.ufpb.dce.poo-aps.Biblioteca/src/br/ufpb/dce/poo/projetopack/Aluno.java
+++ br.ufpb.dce.poo-aps.Biblioteca/src/br/ufpb/dce/poo/projetopack/Aluno.java
@@ -1,18 +1,33 @@
 package br.ufpb.dce.poo.projetopack;
 
-public class Aluno extends UsuarioComposto {
-
+public class Aluno extends Usuario {
 	
+	private String periodoIngresso;
+	private String curso;
 	
 	public Aluno(String nome, String matricula, String cpf, String curso, String periodoIngresso){
 		super(nome, matricula, cpf);
-		super.setCurso(curso);
-		super.setPeriodoIngresso(periodoIngresso);
+		this.curso = curso;
+		this.periodoIngresso = periodoIngresso;
 	}
 
 	public int getQuantDiasEmprestimo() {
 		return Configuracao.getInstance().getDiasEmprestimoAluno();
 	}
 	
+	public void setPeriodoIngresso(String periodo){
+		this.periodoIngresso = periodo;
+	}
 	
+	public String getPeriodoIngresso(){
+		return this.periodoIngresso;	
+	}
+	
+	public void setCurso(String curso){
+		this.curso = curso;
+	}
+	
+	public String getCurso(){
+		return this.curso;
+	}
 }
